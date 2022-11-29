01f54e60441788f68244476f81a2a2440a800400
zubovanastasia <zubovanastasiaa@mail.ru>
2022-11-29 12:05:14 +0300

    Testing

--- IceAndFire(SwiftUI)Tests/IceAndFire_SwiftUI_Tests.swift
+++ IceAndFire(SwiftUI)Tests/IceAndFire_SwiftUI_Tests.swift
@@ -11,74 +11,60 @@ import Combine
 
 class IceAndFire_SwiftUI_Tests: XCTestCase {
     
-    var viewModel: IceAndFireViewModel!
-    var subscriptions = Set<AnyCancellable>()
+    private var mockVM: MockIceAndFireViewModel!
+    private var viewModel: IceAndFireViewModel!
+    private var cancellables:  Set<AnyCancellable> = []
     
     override func setUp() {
-        viewModel = IceAndFireViewModel()
+        viewModel = .init()
     }
     override func tearDown() {
-        subscriptions = []
+        mockVM = nil
+        viewModel = nil
+        cancellables.removeAll()
+        super.tearDown()
     }
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testTags() {
+        let tags: [Tag] = [.alive, .male]
+        var result: [Tag] = []
+        let sub = viewModel.$filterTags
+            .sink(receiveValue: {result = $0})
+        viewModel.filterTags = tags
+        XCTAssertEqual(result, tags)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testTitle() {
+        let tags: [Tag] = [.alive, .male]
+        let expectedTitle = tags.map { $0.rawValue }.joined(separator: "+")
+        viewModel.filterTags = tags
+        XCTAssertEqual(expectedTitle, viewModel.title)
     }
-    
-    func testCollect() {
-        let values = [0, 1, 2]
-        let publisher = values.publisher
-        publisher
-            .collect()
-            .sink(receiveValue: {
-                XCTAssert(
-                $0 == values,
-                "Result was expected to be \(values) but was \($0)"
-                )
-            })
-            .store(in: &subscriptions)
-    }
-    func testFlatMap() {
-        let subj1 = PassthroughSubject<Int, Never>()
-        let subj2 = PassthroughSubject<Int, Never>()
-        let subj3 = PassthroughSubject<Int, Never>()
-        let publisher = CurrentValueSubject<PassthroughSubject<Int, Never>, Never>(subj1)
-        let expected = [1, 2, 4]
-        var result = [Int]()
+    func testFiltering() {
+        let mockView = MockIceAndFireViewModel()
+        mockView.getObject()
         
-        publisher
-            .flatMap(maxPublishers:
-                    .max(2)) { $0 }
-            .sink(receiveValue: { result.append($0)})
-            .store(in: &subscriptions)
-        subj1.send(1)
-        publisher.send(subj2)
-        publisher.send(subj3)
-        subj3.send(3)
-        subj2.send(4)
-        publisher.send(completion: .finished)
-        XCTAssert(
-        result == expected,
-        "Results rxpected to be \(expected) but were \(result)"
-        )
+        let tags: [Tag] = [.male]
+        var result: [IceAndFireCharacters] = []
+        let filteredCharacters  = CharacterStore.getStoreCharacters()
+            .filter {
+                tags.map(\.rawValue.localizedLowercase)
+                .contains($0.gender.lowercased())
+            }
+        let exp = expectation(description: "exp")
+        
+        let sub = mockVM.$filterTags
+            .receive(on: DispatchQueue.main)
+            .sink { newTags in
+                result = mockView.character
+            }
+        mockView.filterTags = tags
+        XCTWaiter.wait(for: [exp], timeout: 0.1)
+        XCTAssertEqual(result.count, filteredCharacters.count)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
+
+final class MockIceAndFireViewModel: IceAndFireViewModel {
+    override func getObject() {
+        characters = CharacterStore.getStoreCharacters()
+    }
+}
+
